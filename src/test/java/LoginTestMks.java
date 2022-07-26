558c91321da742e8cb528f6e3609873d46e30732
ykCyber <yusufkenan2604@gmail.com>
2022-07-26 18:48:59 +0200

    I created my own branc LoginTest cases writeen

---
+++ src/test/java/LoginTestMks.java
@@ -0,0 +1,7 @@
+public class LoginTestMks {
+    public static void main(String[] args) {
+        System.out.println("testcase1");
+        System.out.println("testcase2");
+        System.out.println("testcase3");
+    }
+}
