45f3a2dd5e743e6f33414dc18491d92214892a90
ArifKarakas <arifkarakas@gmail.com>
2022-07-26 20:17:25 +0300

    FirstTry

---
+++ src/test/java/Arif.java
@@ -0,0 +1,9 @@
+package test.java;
+
+public class Arif {
+    public static void main(String[] args) {
+        for (int i=1; i>5; i++){
+            System.out.println("Hello GitHub");
+        }
+    }
+}
